5e70187d2925b7ae54c2b2b969ed9523453bd46d
Nozomi-Hirose <g.road.hrs.n@gmail.com>
2022-12-21 00:39:53 +0900

    checkTest

---
+++ Javastudy/checkTest/src/checkTest/checkTest.java
@@ -0,0 +1,32 @@
+public static void main(String[] args) {
+int number[] = { 3, 5, 7, 9, 11, 13, 15}; 
+
+//Q1-1
+//number配列0番目と3番目の合計値を出力しなさい			
+
+System.out.println(number[0]);
+System.out.println(number[3]);
+
+//Q1-2
+//number配列0番目と3番目の合計値を出力しなさい	
+System.out.println(number[0] + number[3]);
+
+//Q1-3
+//number配列1番目と2番目との乗算を出力しなさい
+System.out.println(number[1] * number[2]);
+
+//Q1-4
+//for文を使い配列の中身を全てを出力しなさい			
+for(int i = 0; i < length; i++);
+System.out.println(number[i]);
+
+//Q1-5						
+//参照テーブルに下記データを追加するSQL文を記載してください(回答のSQL文はコメントアウトすること)						
+//id 1234, name Road, entrance_year 2017, section null						
+//SELECT * FROM JOIN ON =
+
+
+//Q1-6						
+//参照テーブルの下記データを削除するSQL文を記載してください(回答のSQL文はコメントアウトすること)						
+//id 0001と0002のものを削除						
+DELECT FROM WHERE id 0001,id =002;
