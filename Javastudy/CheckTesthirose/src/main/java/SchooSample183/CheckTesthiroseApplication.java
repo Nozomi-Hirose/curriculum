bcfa9175bab44f8afb79811dd578fd456b164c61
Nozomi-Hirose <g.road.hrs.n@gmail.com>
2023-02-24 02:10:10 +0900

    CheckTest

---
+++ Javastudy/CheckTesthirose/src/main/java/SchooSample183/CheckTesthiroseApplication.java
@@ -0,0 +1,13 @@
+package SchooSample183;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckTesthiroseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckTesthiroseApplication.class, args);
+	}
+
+}
---
+++ Javastudy/CheckTesthirose/src/test/java/SchooSample183/CheckTesthiroseApplicationTests.java
@@ -0,0 +1,13 @@
+package SchooSample183;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckTesthiroseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
