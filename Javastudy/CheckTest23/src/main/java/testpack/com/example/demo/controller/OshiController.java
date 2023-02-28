8df683a09fcbf94e9b11d0287ce4ae4cde4752a6
Nozomi-Hirose <g.road.hrs.n@gmail.com>
2023-02-28 18:07:55 +0900

    推し活リスト

--- Javastudy/CheckTest23/src/main/java/testpack/com/example/demo/controller/OshiController.java
+++ Javastudy/CheckTest23/src/main/java/testpack/com/example/demo/controller/OshiController.java
@@ -23,107 +23,107 @@ import testpack.com.example.demo.service.OshiService;
 
 @Controller
 public class OshiController {
-	
-    @Autowired
-    OshiService service;
-	
-    @GetMapping("/oshi-list")
-    public String oshiList(Model model) {
 
-        // serviceを使って、本の一覧をDBから取得する
-        List<Oshi> oshiList = service.findAll();
-        // modelに本の一覧を設定して、画面に渡す
-        model.addAttribute("oshiList", oshiList);
-        // bookList.htmlの表示
-        return "oshiList";
-    }
-	
-    /**
-     * 新規登録画面を表示
-     * @param model
-     * @return 新規登録画面
-     */
-    @GetMapping("/oshi-create")
-    public String createOshi(Model model) {
+	@Autowired
+	OshiService service;
 
-        model.addAttribute("oshiForm", new OshiForm());
+	@GetMapping("/oshi-list")
+	public String oshiList(Model model) {
 
-        return "oshiadd";
-    }
+		// serviceを使って、本の一覧をDBから取得する
+		List<Oshi> oshiList = service.findAll();
+		// modelに本の一覧を設定して、画面に渡す
+		model.addAttribute("oshiList", oshiList);
+		// bookList.htmlの表示
+		return "oshiList";
+	}
 
-    /**
-     * データベースに本を登録する
-     * @param bookForm
-     * @param model
-     * @return
-     */
-    @PostMapping("/oshi-create")
-    public String saveOshi(@ModelAttribute @Validated OshiForm oshiForm, BindingResult result,Model model) {
-    	
-    	// バリデーションエラーの場合
-    	if (result.hasErrors()) {
-    		// 新規登録画面に遷移
-    		return "oshiadd";
-    	}
+	/**
+	 * 新規登録画面を表示
+	 * @param model
+	 * @return 新規登録画面
+	 */
+	@GetMapping("/oshi-create")
+	public String createOshi(Model model) {
 
-        // 本を登録する
-        service.insert(oshiForm);
+		model.addAttribute("oshiForm", new OshiForm());
 
-        // 本の一覧表示画面にリダイレクト
-        return "redirect:/oshi-list";
-    }
-    
-    /**
-     * 編集画面を表示する
-     * @param model
-     * @param editBook
-     * @return
-     */
-    @GetMapping("/oshi-edit")
-    public String editOshi(Model model, EditOshiForm editOshi) {
-    		
-        editOshi = service.getOneBook(editOshi.getId());
-        model.addAttribute(editOshi);
-    		
-        return "oshiedit";
-    }
-    
-    /**
-     * 本の情報を更新する
-     * @param editBook
-     * @param result
-     * @param model
-     * @return
-     */
-    @PostMapping("/oshi-edit")
-    public String update(@ModelAttribute @Validated EditOshiForm editOshi, BindingResult result, Model model) {
+		return "oshiadd";
+	}
 
-        // バリデーションエラーの場合
-        if (result.hasErrors()) {
-            // 編集画面に遷移
-            return "oshiedit";
-        }
+	/**
+	 * データベースに本を登録する
+	 * @param bookForm
+	 * @param model
+	 * @return
+	 */
+	@PostMapping("/oshi-create")
+	public String saveOshi(@ModelAttribute @Validated OshiForm oshiForm, BindingResult result,Model model) {
 
-        // 本を更新する
-        service.update(editOshi);
+		// バリデーションエラーの場合
+		if (result.hasErrors()) {
+			// 新規登録画面に遷移
+			return "oshiadd";
+		}
 
-        // 本の一覧画面にリダイレクト
-        return "redirect:/oshi-list";
-    }
-    
-    /**
-     * 本の削除を行う
-     * @param model
-     * @param Book
-     * @return
-     */
-    @GetMapping("/oshi-delete")
-    public String deleteOshi(Model model, Oshi Oshi) {
+		// 本を登録する
+		service.insert(oshiForm);
 
-        // データベースのデータを削除する
-        service.delete(Oshi.getId());
+		// 本の一覧表示画面にリダイレクト
+		return "redirect:/oshi-list";
+	}
 
-        // 本の一覧画面にリダイレクト
-        return "redirect:/oshi-list";
-    }
+	/**
+	 * 編集画面を表示する
+	 * @param model
+	 * @param editBook
+	 * @return
+	 */
+	@GetMapping("/oshi-edit")
+	public String editOshi(Model model, EditOshiForm editOshi) {
+
+		editOshi = service.getOneBook(editOshi.getId());
+		model.addAttribute(editOshi);
+
+		return "oshiedit";
+	}
+
+	/**
+	 * 本の情報を更新する
+	 * @param editBook
+	 * @param result
+	 * @param model
+	 * @return
+	 */
+	@PostMapping("/oshi-edit")
+	public String update(@ModelAttribute @Validated EditOshiForm editOshi, BindingResult result, Model model) {
+
+		// バリデーションエラーの場合
+		if (result.hasErrors()) {
+			// 編集画面に遷移
+			return "oshiedit";
+		}
+
+		// 本を更新する
+		service.update(editOshi);
+
+		// 本の一覧画面にリダイレクト
+		return "redirect:/oshi-list";
+	}
+
+	/**
+	 * 本の削除を行う
+	 * @param model
+	 * @param Book
+	 * @return
+	 */
+	@GetMapping("/oshi-delete")
+	public String deleteOshi(Model model, Oshi Oshi) {
+
+		// データベースのデータを削除する
+		service.delete(Oshi.getId());
+
+		// 本の一覧画面にリダイレクト
+		return "redirect:/oshi-list";
+	}
 }
